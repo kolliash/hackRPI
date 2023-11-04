62688ae478f75b203867e92174352410b81e23f2
kolliash <143727357+kolliash@users.noreply.github.com>
2023-11-04 15:04:14 -0400

    1

---
+++ project.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+     $("#addNewMed").click(function() {
+      $("#medications").append("<li>medication</li>");
+    });
+
+    $("#med1").click(function(){
+        $("#medsGeneral p").toggle();
+     });
+     $("#AddListItem").click(function() {
+        let i = $("#labList li").length;
+        $("#labList").append("<li>List item " + (i + 1) + "</li>");
+      });
+     // Problem 5 (10 pts): lookup another jquery method and use this code on the "Toggle Text"
+     // link to show/hide the text:
+  
+     $("#toggleText").click(function(){
+        $("#showHideBlock p").toggle();
+     });
+});
